35111fa3e86678a64ad0fca7437f6180268904b4
Florent Machen <florent@machen.fr>
2015-01-25 23:26:36 +0100

    utilisation PDO plutôt que mysqli

--- core/Util/Db.php
+++ core/Util/Db.php
@@ -9,16 +9,10 @@ class Db {
         if (!$conf) {
             throw new Exception('Config error: mysql section missing in config.ini');
         }
-        self::$sql = new mysqli(
-                $conf['host']
-                , $conf['username']
-                , $conf['password']
-                , $conf['dbname']
-                , $conf['port']
-        );
-        if (self::$sql->connect_error) {
-            throw new Exception('Mysql error: (' . $mysqli->connect_errno . ') ' 
-                    . $mysqli->connect_error);
+        try {
+            self::$sql = new PDO($conf['dsn'], $conf['username'], $conf['password']);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
         }
     }
 
