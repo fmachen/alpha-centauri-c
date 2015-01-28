204db8b34fb9d1edf9019a63c400a64808437d32
Florent Machen <florent@machen.fr>
2015-01-28 23:20:47 +0100

    init d'un router basic

--- adm/index.php
+++ adm/index.php
@@ -2,9 +2,22 @@
 
 require '../core/autoload.php';
 
-echo 'adm';
-
 Config::loadFile('../cfg/config.ini');
 
 Db::init();
+
+$user = 'admin';
+
+Router::init();
+Router::map('/foo.*', function() {
+    echo '<h1>FOOOOOO</h1>';
+});
+Router::map('/', function() use ($user) {
+    echo "<h1>Home - $user</h1>";
+});
+Router::run();
+
+// template
+
+echo '<p>adm</p>';
 
---
+++ core/Util/Router.php
@@ -0,0 +1,34 @@
+<?php
+
+class Router {
+
+    private static $path = '';
+    private static $queryString = '';
+    private static $mapping = [];
+
+    public static function init() {
+        self::$path = self::_getRequestPath();
+        self::$queryString = filter_input(INPUT_SERVER, 'QUERY_STRING');
+    }
+
+    private static function _getRequestPath() {
+        $requestUri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+        $delimiter = strpos($requestUri, '?') ? strpos($requestUri, '?') : strlen($requestUri);
+        $scriptDir = dirname(filter_input(INPUT_SERVER, 'SCRIPT_NAME'));
+        return substr($requestUri, strlen($scriptDir), $delimiter);
+    }
+
+    public static function map($pattern, $closure) {
+        self::$mapping[$pattern] = $closure;
+    }
+
+    public static function run() {
+        foreach (self::$mapping as $pattern => $closure) {
+            if (preg_match("#^$pattern$#", self::$path)) {
+                return $closure();
+            }
+        }
+        throw new Exception('Route not Found');
+    }
+
+}
