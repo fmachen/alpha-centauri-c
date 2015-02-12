29938333f804cea9cf4d97dc497937b5ebaf0647
Florent Machen <florent@machen.fr>
2015-02-12 22:33:41 +0100

    404 en cas d'erreur de routing

--- web/index.php
+++ web/index.php
@@ -18,11 +18,10 @@ $app->get('/system/{script}', function ($script) use ($app) {
 });
 $app->get('/{controller}/{method}', function ($controller, $method) use ($app) {
     $object = ucfirst($controller);
-    var_dump($object, class_exists($object));
     if (class_exists($object) && $class = new $object() && method_exists($class, $method)) {
         (new $class())->$method();
     } else {
-        echo "pas glop";
+        $app->abort(404, "Not Found, $object::$method");
     }
 });
 
