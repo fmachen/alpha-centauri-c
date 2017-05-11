d4a9736124da62996b53f69d6d2a2f305d098b0a
fmachen <florent@recisio.com>
2017-05-11 22:35:57 +0200

    init server with socket.io

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/client', express.static(__dirname + '/client'));
+app.listen(8081);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
 
-server.listen(8081, function () {
-    console.log('Listening on ' + server.address().port);
+io.on('connection', function (socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
