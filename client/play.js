48b3e779bcf061363571c42a5b418b2c5c888181
LemoineSimon <lemoine.simon.perso@gmail.com>
2017-05-11 23:38:38 +0200

    Player move when cursors keys is down

--- client/play.js
+++ client/play.js
@@ -5,6 +5,26 @@ var playState = {
         game.load.spritesheet('characters', 'assets/sprites/characters.png', 33, 40);
     },
     create: function(){
-        game.add.sprite(game.width/2,game.height/2,'characters');
+        this.player = game.add.sprite(game.width/2,game.height/2,'characters');
+        game.physics.arcade.enable(this.player);
+        this.cursors = game.input.keyboard.createCursorKeys();
+    },
+    update: function(){
+        if( this.cursors.up.isDown ){
+            this.player.body.y -= 5;
+        }
+
+        if( this.cursors.down.isDown ){
+            this.player.body.y += 5;
+        }
+
+        if( this.cursors.left.isDown ){
+            this.player.body.x -= 5;
+        }
+
+        if( this.cursors.right.isDown ){
+            this.player.body.x += 5;
+        }
     }
+
 }
