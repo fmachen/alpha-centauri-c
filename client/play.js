2722ee06fbc3a44da7dfb4c3a6b4a26acd7b56cc
LemoineSimon <lemoine.simon.perso@gmail.com>
2017-05-11 23:47:40 +0200

    Add player animation

--- client/play.js
+++ client/play.js
@@ -2,28 +2,49 @@
 var playState = {
     preload: function(){
         game.stage.disableVisibilityChange = true;
-        game.load.spritesheet('characters', 'assets/sprites/characters.png', 33, 40);
+        game.load.spritesheet('characters', 'assets/sprites/characters.png', 32, 40);
     },
     create: function(){
+
+        // Add player and set properties
         this.player = game.add.sprite(game.width/2,game.height/2,'characters');
         game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
+        this.player.animations.add('up', [36, 37, 38], 10, true);
+        this.player.animations.add('down', [0, 1, 2], 10, true);
+        this.player.animations.add('left', [12, 13, 14], 10, true);
+        this.player.animations.add('right', [24, 25, 26], 10, true);
+
+        // Create cursors
         this.cursors = game.input.keyboard.createCursorKeys();
     },
     update: function(){
+        this.player.moving = false;
         if( this.cursors.up.isDown ){
+            this.player.moving = true;
             this.player.body.y -= 5;
+            this.player.animations.play('up');
         }
 
         if( this.cursors.down.isDown ){
+            this.player.moving = true;
             this.player.body.y += 5;
+            this.player.animations.play('down');
         }
 
         if( this.cursors.left.isDown ){
+            this.player.moving = true;
             this.player.body.x -= 5;
+            this.player.animations.play('left');
         }
 
         if( this.cursors.right.isDown ){
+            this.player.moving = true;
             this.player.body.x += 5;
+            this.player.animations.play('right');
+        }
+        if( !this.player.moving ){
+            this.player.animations.stop();
         }
     }
 
