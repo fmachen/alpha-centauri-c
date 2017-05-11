21b95161113f8b3e7e1a091af1e749b31b4cfd98
fmachen <florent@recisio.com>
2017-05-11 23:09:13 +0200

    init phaser game

--- client/game.js
+++ client/game.js
@@ -1,5 +1,14 @@
 'use strict';
 
-(function(){
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
-})()
+var game = new Phaser.Game(16 * 32, 600, Phaser.AUTO, document.getElementById('game'));
+game.state.add('Game', Game);
+game.state.start('Game');
+var Game = {};
+
+Game.init = function () {
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function () {
+    game.load.spritesheet('characters', 'assets/sprites/characters.png', 24, 24);
+};
