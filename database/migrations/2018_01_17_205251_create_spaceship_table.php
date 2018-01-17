7eb87e929f14ebc4d815a578464040abe5277c23
fmachen <florent@recisio.com>
2018-01-17 23:35:31 +0100

    add migration for spaceship

---
+++ database/migrations/2018_01_17_205251_create_spaceship_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpaceshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spaceship', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('type_ref');
+            $table->foreign('type_ref', 'type')->references('id')->on('spaceship_type');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spaceship');
+    }
+}
