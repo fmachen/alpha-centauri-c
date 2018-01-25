0eeff2393fcbe1b8bd96eb3bad9d2c2a83354917
fmachen <florent@recisio.com>
2018-01-25 23:42:12 +0100

    table spaceship add user_ref column

--- database/migrations/2018_01_17_205251_create_spaceship_table.php
+++ database/migrations/2018_01_17_205251_create_spaceship_table.php
@@ -15,6 +15,8 @@ class CreateSpaceshipTable extends Migration
     {
         Schema::create('spaceship', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_ref');
+            $table->foreign('user_ref', 'user')->references('id')->on('users');
             $table->unsignedInteger('type_ref');
             $table->foreign('type_ref', 'type')->references('id')->on('spaceship_type');
             $table->string('name');
