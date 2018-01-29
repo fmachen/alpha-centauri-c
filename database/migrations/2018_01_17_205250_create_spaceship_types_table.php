6e7155156333355f8678fe2dbc73be1a254f7cf3
fmachen <florent@recisio.com>
2018-01-29 23:48:36 +0100

    use plural for table name

--- database/migrations/2018_01_17_205250_create_spaceship_types_table.php
+++ database/migrations/2018_01_17_205250_create_spaceship_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSpaceshipTypeTable extends Migration
+class CreateSpaceshipTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSpaceshipTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('spaceship_type', function (Blueprint $table) {
+        Schema::create('spaceship_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->unique('name');
@@ -27,6 +27,6 @@ class CreateSpaceshipTypeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('spaceship_type');
+        Schema::dropIfExists('spaceship_types');
     }
 }
--- database/migrations/2018_01_17_205251_create_spaceships_table.php
+++ database/migrations/2018_01_17_205251_create_spaceships_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSpaceshipTable extends Migration
+class CreateSpaceshipsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateSpaceshipTable extends Migration
      */
     public function up()
     {
-        Schema::create('spaceship', function (Blueprint $table) {
+        Schema::create('spaceships', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_ref');
             $table->foreign('user_ref', 'user')->references('id')->on('users');
             $table->unsignedInteger('type_ref');
-            $table->foreign('type_ref', 'type')->references('id')->on('spaceship_type');
+            $table->foreign('type_ref', 'type')->references('id')->on('spaceship_types');
             $table->string('name');
             $table->timestamps();
         });
@@ -31,6 +31,6 @@ class CreateSpaceshipTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('spaceship');
+        Schema::dropIfExists('spaceships');
     }
 }
