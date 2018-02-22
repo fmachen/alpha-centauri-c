b314678e592806a3f19e870e78c5a3812080ff79
LemoineSimon <lemoine.simon.perso@gmail.com>
2018-02-22 22:05:42 +0100

    Add spaceship starter hull and spaceship room type icons

--- resources/views/spaceship/build.blade.php
+++ resources/views/spaceship/build.blade.php
@@ -5,9 +5,11 @@
         <h1>Build your spaceship</h1>
 
         <div>
-            <div class="spaceship">
-                <div class="slot" data-type="motor"></div>
-                <div class="slot" data-type="energy"></div>
+            <div class="spaceship" data-type="starter">
+                <div class="slot" data-type="engine" data-size="2"></div>
+                <div class="slot" data-type="cockpit" data-size="1"></div>
+                <div class="slot" data-type="supply" data-size="0"></div>
+                <div class="slot" data-type="" data-size="0"></div>
             </div>
         </div>
     </div>
