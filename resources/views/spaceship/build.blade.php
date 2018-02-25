da6ef3c630a78066d0799b604ae0c23d3c90f617
LemoineSimon <lemoine.simon.perso@gmail.com>
2018-02-25 16:56:26 +0100

    Petit tableau des specs

--- resources/views/spaceship/build.blade.php
+++ resources/views/spaceship/build.blade.php
@@ -3,13 +3,39 @@
 @section('content')
     <div class="container">
         <h1>Build your spaceship</h1>
-
-        <div>
+        <div class="flex">
             <div class="spaceship" data-type="starter">
                 <div class="slot" data-type="engine" data-size="2"></div>
                 <div class="slot" data-type="cockpit" data-size="1"></div>
                 <div class="slot" data-type="supply" data-size="0"></div>
                 <div class="slot" data-type="" data-size="0"></div>
+            </div>
+            <div class="prm plm">
+                <table class="spaceship__specification">
+                    <thead>
+                    <tr>
+                        <td colspan="2">Specifications</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Cockpit</td>
+                        <td>Level 1</td>
+                    </tr>
+                    <tr>
+                        <td>Motor</td>
+                        <td>Level 1</td>
+                    </tr>
+                    <tr>
+                        <td>Supply</td>
+                        <td>Level 1</td>
+                    </tr>
+                    <tr>
+                        <td>Weight</td>
+                        <td>20 Tons</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
